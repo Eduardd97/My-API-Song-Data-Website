2657e24cd46e68225d1b6825d9866d96c94fc9e5
Eduard <kozeychukk1997@gmail.com>
2024-04-21 18:52:11 +0300

    search-from-history-by-name-in-lokal-storage

--- main.js
+++ main.js
@@ -51,11 +51,22 @@ const searchInput = document.querySelector("#search-input");
 const searchButton = document.querySelector("#search-button");
 const songsCardsWrapper = document.querySelector(".music-cards-wrapper");
 const currentSongName = document.querySelector("#current-song-name");
+const positionPlayerControlls = document.querySelector(".player-controlls");
+const responsiveCurrentSongName = document.querySelector(
+    "#responsive-current-song-name"
+);
 const volumeSlider = document.getElementById("volumeSlider");
+
+const mediaPlayerButtons = {
+    playButton: document.getElementById("play-btn"),
+    previousButton: document.getElementById("previous-btn"),
+    nextButton: document.getElementById("next-btn"),
+};
 
 // Array of media data
 
-let mediaData = [];
+let songs = [];
+// let currentMediaIndex = 0;
 
 // API
 
@@ -82,14 +93,14 @@ class API {
             headers: this.options.headers,
         });
 
-        mediaData = response.data.data;
-        console.log(mediaData);
+        songs = response.data.data;
 
         return response.data;
     }
 }
 
 const musicAPI = new API("https://deezerdevs-deezer.p.rapidapi.com");
+musicAPI.searchSongByArtist("");
 
 class Views {
     renderSongCard(songData = songInterface, parent) {
@@ -125,19 +136,15 @@ class Views {
         audioButton.className = "audio-button";
 
         audioButton.onclick = () => {
-            if (audio.src === songData.preview) {
-                audio.src = "";
-                audio.pause();
-                audioButton.textContent = "Stop!";
-            } else {
-                audio.src = songData.preview;
-                audio.load();
-                audio.play();
+            audio.src = songData.preview;
+            audio.load();
 
-                views.updateCurrentSongName(songData);
+            Player.controlls.play.classList.replace("fa-play", "fa-pause");
+            mediaPlayerButtons.playButton.src =
+                "./image/svg/mediaPlayerButtons/pauseButton.svg";
+            audio.play();
 
-                audioButton.textContent = "Play!";
-            }
+            views.updateCurrentSongName(songData);
         };
 
         cardWrapper.appendChild(audioButton);
@@ -147,39 +154,138 @@ class Views {
     }
 
     updateCurrentSongName(songData) {
-        currentSongName.textContent = `${songData.title} (${getSongDuration(
-            songData.duration
-        )})`;
+        if (innerWidth < 450) {
+            responsiveCurrentSongName.textContent = `${
+                songData.title
+            } (${getSongDuration(songData.duration)})`;
+        } else {
+            currentSongName.textContent = `${songData.title} (${getSongDuration(
+                songData.duration
+            )})`;
+        }
     }
 }
 
 const views = new Views();
 
+class SearchHistory {
+    getHistory() {
+        return JSON.parse(localStorage.getItem("history") || "[]");
+    }
+
+    addToHistory(request) {
+        let history = this.getHistory();
+
+        if (history.includes(request)) {
+            history = history.filter((prevRequest) => prevRequest !== request);
+        }
+
+        history.push(request.toLowerCase());
+
+        localStorage.setItem("history", JSON.stringify(history));
+    }
+
+    renderHistryBySearch() {
+        const searchFromHistoryByName = document.getElementById(
+            "search-from-history-by-name"
+        );
+
+        searchFromHistoryByName.innerHTML = "";
+
+        const getHistory = this.getHistory();
+
+        let maxButtonsToShow = 0;
+
+        innerWidth < 450 ? maxButtonsToShow = 3 : maxButtonsToShow = 6;
+ 
+        // Перемещаем последние выбранные элементы в конец массива
+        const lastTitles = getHistory.slice(-maxButtonsToShow);
+        // console.log(lastTitles);
+        // lastTitles.map((title) => {
+        //     const index = getHistory.indexOf(title);
+        //     if (index !== -1) {
+        //         getHistory.splice(index, 1);
+        //         getHistory.push(title);
+        //     }
+
+        //     return lastTitles;
+        // });
+
+        lastTitles.forEach((title) => {
+            const buttonSearchByHistory = createButton(title);
+            searchFromHistoryByName.appendChild(buttonSearchByHistory);
+        });
+
+        function createButton(songTitle) {
+            const buttonSearchByHistory = document.createElement("button");
+            buttonSearchByHistory.className = "button-search-by-history";
+            buttonSearchByHistory.textContent = songTitle.toLowerCase();
+
+            async function searchByHistory() {
+                const searchValue = songTitle;
+                const historySongsTitleData = await musicAPI.searchSongByArtist(
+                    searchValue
+                );
+                console.log(historySongsTitleData);
+
+                songsCardsWrapper.innerHTML = "";
+
+                historySongsTitleData.data.forEach((songData) => {
+                    views.renderSongCard(songData, songsCardsWrapper);
+                });
+            }
+
+            buttonSearchByHistory.onclick = searchByHistory;
+
+            return buttonSearchByHistory;
+        }
+    }
+}
+
+const seachHistory = new SearchHistory();
+
 async function renderSongsCards() {
-    const searchValue = searchInput.value;
+    if (searchInput.value === "") {
+        console.log("search cannot be empty");
+    } else {
+        const searchValue = searchInput.value;
 
-    const songsData = await musicAPI.searchSongByArtist(searchValue);
+        seachHistory.addToHistory(searchInput.value);
 
-    songsCardsWrapper.innerHTML = "";
+        const songsData = await musicAPI.searchSongByArtist(searchValue);
 
-    songsData.data.forEach((songData) => {
-        views.renderSongCard(songData, songsCardsWrapper);
-    });
+        songsCardsWrapper.innerHTML = "";
 
-    searchInput.value = "";
+        songsData.data.forEach((songData) => {
+            views.renderSongCard(songData, songsCardsWrapper);
+        });
+
+        searchInput.value = "";
+    }
 
     // console.log(songsData);
 }
 
-searchButton.onclick = renderSongsCards;
+searchButton.onclick = () => {
+    renderSongsCards();
+    if (searchInput.value === "") {
+        console.log("search cannot be empty");
+    } else {
+        seachHistory.renderHistryBySearch();
+    }
+};
+
+function normalizeTimeValue(timeValue) {
+    return timeValue.length === 2 ? timeValue : `0${timeValue}`;
+}
 
 function getSongDuration(s) {
     const date = new Date();
     date.setHours(0, 0, 0, 0);
     date.setSeconds(s);
 
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
+    const minutes = normalizeTimeValue(`${date.getMinutes()}`);
+    const seconds = normalizeTimeValue(`${date.getSeconds()}`);
 
     return `${minutes}:${seconds}`;
 }
@@ -188,68 +294,201 @@ function setVolume() {
     audio.volume = volumeSlider.value;
 }
 
+// Устанавливаем громкость в начальное значение при загрузке страницы
 setVolume();
 
+// Обрабатываем событие изменения положения ползунка
 volumeSlider.addEventListener("input", setVolume);
 
-// Завдання: реалізуйте управління плеєром
-// Тільки кнопки: вперед, назад, стоп, старт
+// Player
 
-// Кнопки мають завантажувати наступну/попередню пісню
+// Player Buttons
 
-// Якщо пісень немає - кнопки ігнорують натискання
-
-function mediaPlayer(currentMediaIndex = 0) {
-    const mediaPlayerButtons = {
-        playButton: document.getElementById("play-btn"),
-        previousButton: document.getElementById("previous-btn"),
-        nextButton: document.getElementById("next-btn"),
+class Player {
+    static controlls = {
+        prev: document.getElementById("prev-song-button"),
+        play: document.getElementById("play-stop-button"),
+        next: document.getElementById("next-song-button"),
+        progress: document.getElementById("player-progress"),
+        progresTimer: document.getElementById("player-progress-time"),
     };
 
+    constructor(
+        mediaPlayerButtons,
+        parentSongName,
+        responsiveParentSongName,
+        currentSongIndex = 0
+    ) {
+        this.currentSongIndex = currentSongIndex;
+        this.mediaPlayerButtons = mediaPlayerButtons;
+        this.parentSongName = parentSongName;
+        this.responsiveParentSongName = responsiveParentSongName;
+
+        Player.controlls.next.onclick = () => {
+            this.nextSong();
+        };
+
+        Player.controlls.prev.onclick = () => {
+            this.prevSong();
+        };
+
+        Player.controlls.play.onclick = () => {
+            if (!audio.paused) {
+                this.stop();
+            } else {
+                audio.src = songs[this.currentSongIndex].preview;
+                if (innerWidth < 450) {
+                    this.responsiveParentSongName.textContent = `${
+                        songs[this.currentSongIndex].title
+                    } (${getSongDuration(songs[this.currentSongIndex].duration)})`;
+                } else {
+                    this.parentSongName.textContent = `${
+                        songs[this.currentSongIndex].title
+                    } (${getSongDuration(songs[this.currentSongIndex].duration)})`;
+                }
+                this.play();
+            }
+        };
+
+        Player.controlls.progress.oninput = () => {
+            audio.currentTime = +Player.controlls.progress.value;
+        };
+
+        audio.ontimeupdate = () => {
+            Player.controlls.progress.value = audio.currentTime;
+            Player.controlls.progresTimer.textContent = getSongDuration(
+                audio.currentTime
+            );
+        };
+    }
+
+    stop() {
+        Player.controlls.play.classList.replace("fa-pause", "fa-play");
+        this.mediaPlayerButtons.playButton.src =
+            "./image/svg/mediaPlayerButtons/playButton.svg";
+        audio.pause();
+    }
+    play() {
+        Player.controlls.play.classList.replace("fa-play", "fa-pause");
+        this.mediaPlayerButtons.playButton.src =
+            "./image/svg/mediaPlayerButtons/pauseButton.svg";
+        Player.controlls.progress.max = songs[this.currentSongIndex].duration;
+        audio.play();
+    }
+    prevSong() {
+        if (songs.length) {
+            Player.controlls.progress.value = 0;
+            this.currentSongIndex -= 1;
+            audio.src =
+                songs[this.currentSongIndex]?.preview || songs[0].preview;
+            this.play();
+
+            if (innerWidth < 450) {
+                this.responsiveParentSongName.textContent = `${
+                    songs[this.currentSongIndex].title
+                } (${getSongDuration(songs[this.currentSongIndex].duration)})`;
+            } else {
+                this.parentSongName.textContent = `${
+                    songs[this.currentSongIndex].title
+                } (${getSongDuration(songs[this.currentSongIndex].duration)})`;
+            }
+        }
+    }
+    nextSong() {
+        if (songs.length) {
+            Player.controlls.progress.value = 0;
+            this.currentSongIndex += 1;
+            audio.src =
+                songs[this.currentSongIndex]?.preview || songs[0].preview;
+            this.play();
+
+            if (innerWidth < 450) {
+                this.responsiveParentSongName.textContent = `${
+                    songs[this.currentSongIndex].title
+                } (${getSongDuration(songs[this.currentSongIndex].duration)})`;
+            } else {
+                this.parentSongName.textContent = `${
+                    songs[this.currentSongIndex].title
+                } (${getSongDuration(songs[this.currentSongIndex].duration)})`;
+            }
+        }
+    }
+}
+
+const player = new Player(
+    mediaPlayerButtons,
+    currentSongName,
+    responsiveCurrentSongName
+);
+
+function mediaPlayer(currentMediaIndex = 0) {
     mediaPlayerButtons.playButton.onclick = () => {
-        if (mediaData.length) {
+        if (songs.length) {
             if (!audio.paused) {
                 mediaPlayerButtons.playButton.src =
-                    "./image/svg/mediaPlayerButtons/pauseButton.svg";
+                    "./image/svg/mediaPlayerButtons/playButton.svg";
+                Player.controlls.play.classList.replace("fa-pause", "fa-play");
                 audio.pause();
             } else {
                 mediaPlayerButtons.playButton.src =
-                    "./image/svg/mediaPlayerButtons/playButton.svg";
-                audio.src = mediaData[currentMediaIndex].preview;
+                    "./image/svg/mediaPlayerButtons/pauseButton.svg";
+                Player.controlls.play.classList.replace("fa-play", "fa-pause");
+                audio.src = songs[currentMediaIndex].preview;
                 audio.play();
-                currentSongName.textContent = `${
-                    mediaData[currentMediaIndex].title
-                } (${getSongDuration(mediaData[currentMediaIndex].duration)})`;
+                if (innerWidth < 450) {
+                    responsiveCurrentSongName.textContent = `${
+                        songs[currentMediaIndex].title
+                    } (${getSongDuration(songs[currentMediaIndex].duration)})`;
+                } else {
+                    currentSongName.textContent = `${
+                        songs[currentMediaIndex].title
+                    } (${getSongDuration(songs[currentMediaIndex].duration)})`;
+                }
             }
         }
     };
 
     mediaPlayerButtons.previousButton.onclick = () => {
-        if (mediaData.length) {
+        if (songs.length) {
             currentMediaIndex -= 1;
-            audio.src = mediaData[currentMediaIndex].preview;
-            audio.play();
-            if (audio.played) {
+            audio.src = songs[currentMediaIndex].preview;
+            if (!audio.paused) {
                 mediaPlayerButtons.playButton.src =
                     "./image/svg/mediaPlayerButtons/playButton.svg";
+                Player.controlls.play.classList.replace("fa-pause", "fa-play");
             }
-            currentSongName.textContent = `${
-                mediaData[currentMediaIndex].title
-            } (${getSongDuration(mediaData[currentMediaIndex].duration)})`;
+            audio.play();
+            if (innerWidth < 450) {
+                responsiveCurrentSongName.textContent = `${
+                    songs[currentMediaIndex].title
+                } (${getSongDuration(songs[currentMediaIndex].duration)})`;
+            } else {
+                currentSongName.textContent = `${
+                    songs[currentMediaIndex].title
+                } (${getSongDuration(songs[currentMediaIndex].duration)})`;
+            }
         }
     };
     mediaPlayerButtons.nextButton.onclick = () => {
-        if (mediaData.length) {
+        if (songs.length) {
             currentMediaIndex += 1;
-            audio.src = mediaData[currentMediaIndex].preview;
-            audio.play();
-            if (audio.played) {
+            audio.src = songs[currentMediaIndex].preview;
+            if (!audio.paused) {
                 mediaPlayerButtons.playButton.src =
                     "./image/svg/mediaPlayerButtons/playButton.svg";
+                Player.controlls.play.classList.replace("fa-pause", "fa-play");
             }
-            currentSongName.textContent = `${
-                mediaData[currentMediaIndex].title
-            } (${getSongDuration(mediaData[currentMediaIndex].duration)})`;
+            audio.play();
+            if (innerWidth < 450) {
+                responsiveCurrentSongName.textContent = `${
+                    songs[currentMediaIndex].title
+                } (${getSongDuration(songs[currentMediaIndex].duration)})`;
+            } else {
+                currentSongName.textContent = `${
+                    songs[currentMediaIndex].title
+                } (${getSongDuration(songs[currentMediaIndex].duration)})`;
+            }
+
         }
     };
 }
